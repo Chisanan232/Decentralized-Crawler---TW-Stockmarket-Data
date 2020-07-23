e7266fe4aa11bb036be767357eef3623814d8f63
Chisanan232 <chi10211201@cycu.org.tw>
2020-07-23 22:48:04 +0800

    Remove some useless AKKA configuration

--- src/main/scala/config/AkkaConfig.scala
+++ src/main/scala/config/AkkaConfig.scala
@@ -4,7 +4,6 @@ object AkkaConfig {
 
   val SystemName = "CauchySystem"
   val KingName = "King"
-  var KingPath = ""
 
   val CrawlerNumber = 2
   val ProducerNumber = 2
@@ -15,15 +14,10 @@ object AkkaConfig {
      * Parameters about crawler part.
      */
 
-    val CrawlPremierName = "CrawlerPremier"
-    var PremierPath = ""
-    val CrawlerPaladinName = "CrawlerPaladin"
-    var CrawlerPaladinPath = ""
-    val CrawlSoldierName = "Soldier_"
-    var CrawlSoldierPaths = new Array[String](CrawlerNumber)
+    val PaladinName = "CrawlerPaladin"
+    val SoldierName = "Soldier_"
     val ConsumerGroupID = "crawler"
     val DataSaverName = ""
-    val DataSaverPaths = new Array[String](CrawlerNumber)
   }
 
 
@@ -32,31 +26,17 @@ object AkkaConfig {
      * Parameters about data analyser part.
      */
 
-    val DataPremierName = "DataPremier"
-    var PremierPath = ""
-
     val ProducerPaladinName = "KafkaProducerLeader"
-    var ProducerPaladinPath = ""
     val ProducerSoldierName = "producer_"
-    var ProducerSoldierPaths = new Array[String](KafkaConfig.APIsTopicPartitionsNum)
 
-    val ConsumerPaladinName = "KafkaConsumerLeader"
-    var ConsumerPaladinPath = ""
-    val ConsumerSoldierName = "consumer_"
-    var ConsumerSoldierPaths = new Array[String](CrawlerNumber)
+    val SearchPaladinName = "KafkaConsumerLeader"
+    val SearchSoldierName = "consumer_"
 
     val ProducerDateSoldierName = "produce_date_soldier"
-    var ProducerDateSoldierPath = ""
     val ProducerStockSymbolsSoldierName = "produce_symbol_soldier"
-    var ProducerStockSymbolsSoldierPath = ""
 
-    val ConsumerDateSoldierName = "consumer_date_soldier"
-    var ConsumerDateSoldierPath = ""
-    val ConsumerStockSymbolsSoldierName = "consumer_symbol_soldier"
-    var ConsumerStockSymbolsSoldierPath = ""
-
-    val ExamineSoldierName = "examiner_"  // This is consumer team
-    var ExamineSoldierPaths = new Array[String](KafkaConfig.DateTimeTopicPartitionsNum)
+    val SearchDateSoldierName = "consumer_date_soldier"
+    val SearchStockSymbolsSoldierName = "consumer_symbol_soldier"
   }
 
 }
