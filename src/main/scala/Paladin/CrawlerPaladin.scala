cf0589fabd0d643728c5722ffb594213bc0c3e48
Chisanan232 <chi10211201@cycu.org.tw>
2020-07-24 15:36:53 +0800

    Refector code and add new logic implement and message

--- src/main/scala/Paladin/CrawlerPaladin.scala
+++ src/main/scala/Paladin/CrawlerPaladin.scala
@@ -4,12 +4,32 @@ import Taiwan_stock_market_crawler_Cauchy.src.main.scala.CheckMechanism
 import Taiwan_stock_market_crawler_Cauchy.src.main.scala.Soldier.CrawlerSoldier
 import Taiwan_stock_market_crawler_Cauchy.src.main.scala.config._
 
+import scala.collection.mutable.ListBuffer
+
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 
 
 class CrawlerPaladin extends Actor with ActorLogging {
 
+  /** The logic of actors state Map value is "Does it could receive next task?" **/
+  var actorsState: Map[String, Boolean] = Map[String, Boolean]()
+  var leftAPIMap: Map[String, ListBuffer[String]] = Map[String, ListBuffer[String]]()
+
   private val check = new CheckMechanism
+
+  private def initActorsState(actorName: String): Map[String, Boolean] = {
+    actorsState += (AkkaConfig.CrawlerDepartment.SoldierName -> true)
+    actorsState
+  }
+
+  private def recordHistory(key: String, date: String): Map[String, ListBuffer[String]] = {
+    /*
+    Update the datetime data after examiner soldiers get the data every time
+     */
+    val newDateArray = this.leftAPIMap(key).filter(!_.equals(date))
+    leftAPIMap += (key -> newDateArray)
+    leftAPIMap
+  }
 
   override def receive: Receive = {
 
@@ -26,11 +46,32 @@ class CrawlerPaladin extends Actor with ActorLogging {
       log.info("\uD83D\uDCAA Okay, come on, my greatest crawler soldiers. Go work!")
 
       val crawlerSoldiers = new Array[ActorRef](AkkaConfig.CrawlerNumber)
-      for (crawlerID <- 0.until(AkkaConfig.CrawlerNumber)) crawlerSoldiers(crawlerID) = context.actorOf(Props[CrawlerSoldier], AkkaConfig.CrawlerDepartment.SoldierName + crawlerID.toString)
+      for (crawlerID <- 0.until(AkkaConfig.CrawlerNumber)) {
+        val actorName = AkkaConfig.CrawlerDepartment.SoldierName + crawlerID.toString
+        crawlerSoldiers(crawlerID) = context.actorOf(Props[CrawlerSoldier], actorName)
+        this.initActorsState(actorName)
+      }
       crawlerSoldiers.foreach(crawlerSoldierRef => {
         val soldier = context.actorSelection(crawlerSoldierRef.path)
         soldier ! ReadyOnStandBy
+        actorsState += (crawlerSoldierRef.path.name -> false)
       })
+
+
+    /** Check actor status and distribute tasks to soldiers **/
+    case GotAPI(content, api) =>
+      log.info("\uD83D\uDCEC Got API.")
+      for (crawlerID <- 0.until(AkkaConfig.CrawlerNumber)) {
+        val actorName = AkkaConfig.CrawlerDepartment.SoldierName + crawlerID.toString
+        if (actorsState(actorName).equals(true)) {
+          // Send task to it
+          context.actorSelection(actorName) ! TargetAPI("\uD83D\uDCE9 Here is API info!", api)
+        } else {
+          // Save task to variable
+          val latestAPIs = leftAPIMap(actorName) += api
+          leftAPIMap += (actorName -> latestAPIs)
+        }
+      }
 
   }
 
