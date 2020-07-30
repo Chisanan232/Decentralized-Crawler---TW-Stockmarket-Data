08561ac99b6d51e8c934d4117c0a52adf8204c57
Chisanan232 <chi10211201@cycu.org.tw>
2020-07-30 14:01:58 +0800

    Receive parameter about whether program uses docker or not

--- src/main/scala/Cauchy.scala
+++ src/main/scala/Cauchy.scala
@@ -6,10 +6,20 @@ import Taiwan_stock_market_crawler_Cauchy.src.main.scala.King.CauchyKing
 import akka.actor.{ActorSystem, Props}
 
 
-object Cauchy extends App {
+object Cauchy {
 
-  val system = ActorSystem(AkkaConfig.SystemName)
-  val KingActor = system.actorOf(Props[CauchyKing], AkkaConfig.KingName)
-  KingActor ! DataAim
+  def main(args: Array[String]): Unit = {
+    val UsingDockerEnv = args(0)
+    UsingDockerEnv.toString match {
+      case "enable" => AkkaConfig.InDocker = true
+      case "disable" => AkkaConfig.InDocker = false
+      case _ => AkkaConfig.InDocker = false
+    }
+    println(s"[INFO] The environment variable 'UseDockerEnv' value is $UsingDockerEnv")
+
+      val system = ActorSystem(AkkaConfig.SystemName)
+      val KingActor = system.actorOf(Props[CauchyKing], AkkaConfig.KingName)
+      KingActor ! DataAim
+  }
 
 }
