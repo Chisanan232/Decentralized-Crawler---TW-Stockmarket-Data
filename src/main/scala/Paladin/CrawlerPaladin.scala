9111f582c2e07fab45cda92761e8bd4be48d93d0
Chisanan232 <chi10211201@cycu.org.tw>
2020-07-30 15:36:21 +0800

    Add a new received-message to handle left APIs

--- src/main/scala/Paladin/CrawlerPaladin.scala
+++ src/main/scala/Paladin/CrawlerPaladin.scala
@@ -1,10 +1,10 @@
 package Taiwan_stock_market_crawler_Cauchy.src.main.scala.Paladin
 
-import Taiwan_stock_market_crawler_Cauchy.src.main.scala.CheckMechanism
 import Taiwan_stock_market_crawler_Cauchy.src.main.scala.Soldier.CrawlerSoldier
 import Taiwan_stock_market_crawler_Cauchy.src.main.scala.config._
 
 import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks.{break, breakable}
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 
@@ -12,10 +12,10 @@ import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 class CrawlerPaladin extends Actor with ActorLogging {
 
   /** The logic of actors state Map value is "Does it could receive next task?" **/
+  var currentReceiveNum: BigInt = _
+  var allNum: BigInt = _
   var actorsState: Map[String, Boolean] = Map[String, Boolean]()
   var leftAPIMap: Map[String, ListBuffer[String]] = Map[String, ListBuffer[String]]()
-
-  private val check = new CheckMechanism
 
   private def initActorsState(actorName: String): Map[String, Boolean] = {
     actorsState += (AkkaConfig.CrawlerDepartment.SoldierName -> true)
@@ -26,7 +26,7 @@ class CrawlerPaladin extends Actor with ActorLogging {
     /*
     Update the datetime data after examiner soldiers get the data every time
      */
-    val newDateArray = this.leftAPIMap(key).filter(!_.equals(date))
+    val newDateArray = leftAPIMap(key).filter(!_.equals(date))
     leftAPIMap += (key -> newDateArray)
     leftAPIMap
   }
@@ -42,7 +42,8 @@ class CrawlerPaladin extends Actor with ActorLogging {
 
 
     /** Build up crawler soldiers (in other words, they're crawler threads) **/
-    case AwaitDataAndCrawl =>
+    case AwaitDataAndCrawl(content, allTaskNum) =>
+      allNum = allTaskNum
       log.info("\uD83D\uDCAA Okay, come on, my greatest crawler soldiers. Go work!")
 
       val crawlerSoldiers = new Array[ActorRef](AkkaConfig.CrawlerNumber)
@@ -60,6 +61,7 @@ class CrawlerPaladin extends Actor with ActorLogging {
 
     /** Check actor status and distribute tasks to soldiers **/
     case GotAPI(content, api) =>
+      currentReceiveNum += 1
       log.info("\uD83D\uDCEC Got API.")
       for (crawlerID <- 0.until(AkkaConfig.CrawlerNumber)) {
         val actorName = AkkaConfig.CrawlerDepartment.SoldierName + crawlerID.toString
@@ -72,12 +74,45 @@ class CrawlerPaladin extends Actor with ActorLogging {
           leftAPIMap += (actorName -> latestAPIs)
         }
       }
+      if (currentReceiveNum.equals(allNum)) {
+        self ! ClearData
+      }
 
 
-    case FinishCurrentJob(content, actor) =>
-      log.info(s"$content from AKKA actor $actor")
-      actorsState += (actor -> true)
+    /** Handle the all of left APIs **/
+    case ClearData =>
+      log.info("\uD83E\uDDF9 Clean the data if it left.")
+      breakable {
+        while (true) {
+          var clearAll = true
+          for ((actor, leftAPI) <- leftAPIMap) {
+            log.info(s"Checking AKKA actor $actor")
+            if (leftAPI.nonEmpty) {
+              // Send task to actor
+              clearAll = false
+              if (actorsState(actor).equals(true)) {
+                context.actorSelection(actor) ! TargetAPI("\uD83D\uDCE9 Here is API info!", leftAPI.apply(1))
+                this.recordHistory(actor, leftAPI.apply(1))
+              }
+            }
+
+            if (clearAll.equals(true)) {
+              // Break and end this actor
+              log.info("\uD83E\uDDFC Finish to clean all data.")
+              break()
+            }
+
+            // Checking this every 10 seconds.
+            Thread.sleep(10000)
+          }
+        }
+      }
+
+
+        case FinishCurrentJob(content, actor)
+        =>
+        log.info(s"$content from AKKA actor $actor")
+        actorsState += (actor -> true)
 
   }
-
 }
